0a3c6546f5e71ae109e5fd95c41f915467766595
hdungz <52479099+dungz878@users.noreply.github.com>
2025-10-07 14:39:07 +0700

    Add a todo list

--- src/main/java/com/arkanoid/model/paddle/PaddleType.java
+++ src/main/java/com/arkanoid/model/paddle/PaddleType.java
@@ -2,10 +2,10 @@ package com.arkanoid.model.paddle;
 
 public enum PaddleType {
 
-    Default("Default","DefaultPaddle", 90.0, 3.0),      // Cân bằng (90 * 1.0, 3 * 1.0)
-    Aegis("Aegis","AegisPaddle",117.0, 2.7),           // Rộng hơn, phòng thủ tốt, hơi chậm (90 * 1.3, 3 * 0.9)
-    Swift("Swift","SwiftPaddle", 72.0, 3.6 ),            // Hẹp hơn, nhanh nhẹn (90 * 0.8, 3 * 1.2)
-    Titan("Titan", "TitanPaddle",144.0, 2.1);           // Siêu rộng, phòng thủ tối thượng, rất chậm (90 * 1.6, 3 * 0.7)
+    Default("Default","DefaultPaddle", 90.0, 3.0),
+    Aegis("Aegis","AegisPaddle",117.0, 2.7),
+    Swift("Swift","SwiftPaddle", 72.0, 3.6 ),
+    Titan("Titan", "TitanPaddle",144.0, 2.1);
 
     private final String displayName;
     private final double width;
