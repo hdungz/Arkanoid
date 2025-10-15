148bc3d4165e79048d87d1b60c27680974deecbd
hdungz <52479099+dungz878@users.noreply.github.com>
2025-10-15 11:00:52 +0700

    Refactors brick logic and removes comments
    This commit refactors brick behavior for improved gameplay and maintainability.
    
    It removes unnecessary comments from the brick classes, enhancing code clarity.
    Additionally, it adjusts the damage logic for ExplodingBrick and introduces SuperDurableBrick which is immune to explosion.
    
    This change streamlines the codebase and enhances gameplay dynamics.

--- src/main/java/com/arkanoid/model/brick/SuperDurableBrick.java
+++ src/main/java/com/arkanoid/model/brick/SuperDurableBrick.java
@@ -2,6 +2,11 @@ package com.arkanoid.model.brick;
 
 public class SuperDurableBrick extends Brick {
     public SuperDurableBrick(double x, double y, double width, double height, BrickType type) {
-        super(x, y, width, height, type,3);
+        super(x, y, width, height, type,-1);
+    }
+    @Override
+    public boolean takeDamage() {
+
+       return true;
     }
 }
