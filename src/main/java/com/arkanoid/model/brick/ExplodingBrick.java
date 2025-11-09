c79eaa32c7c9910a8155bfe3789a1696f98c0011
whiue-nhh12 <hieuhuu0609@gmail.com>
2025-11-09 22:40:20 +0700

    FinishLevel and Create Level

--- src/main/java/com/arkanoid/model/brick/ExplodingBrick.java
+++ src/main/java/com/arkanoid/model/brick/ExplodingBrick.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 
 public class ExplodingBrick extends Brick {
     protected final GameModel gameModel;
-    private double explosionRadius = 100.0;
+    private double explosionRadius = 70.0;
     private boolean exploding = false;
     private boolean firstHit = true; // Chỉ kêu va chạm lần đầu
 
